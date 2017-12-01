4ceb48757a7fb8edfd485c55d25f33c5ad8fcff8
Joe Maffei <joemaffei@users.noreply.github.com>
2017-12-01 00:11:30 -0600

    cleaned up day 1 part 1 solution

--- src/01-1.js
+++ src/01-1.js
@@ -3,7 +3,10 @@ const data = require('./load')('01');
 const dataArray = data.split('').map(x => +x);
 
 const total = dataArray.reduce((result, current, index) => {
-  return current === dataArray[(index + 1) % dataArray.length] ? result + current : result;
+  const next = dataArray[(index + 1) % dataArray.length];
+  return current === next
+    ? result + current
+    : result;
 }, 0);
 
 console.log(total);
